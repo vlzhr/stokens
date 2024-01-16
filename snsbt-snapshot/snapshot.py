b16437bc3ca566e808a3230aec7e08ddcb4af491
Ekaterino <izhur27@gmail.com>
2024-01-16 15:39:45 +0100

    adding snsbt snapshot

---
+++ snsbt-snapshot/snapshot.py
@@ -0,0 +1,40 @@
+from requests import get
+from json import loads, dumps
+
+pools = loads(get("https://puzzle-js-back.herokuapp.com/api/v1/pools").text)
+result = {"assets": {}, "pools": {}}
+tokens = {"sNSBT": "8wUmN9Y15f3JR4KZfE81XLXpkdgwnqoBNG6NmocZpKQx"} # {"PEPE-NW": "DnsgQ23DYGgRDxUwjmZY4sEZ4QaRtUdLVuzDPBdWJo7G", "WAVES": "WAVES", "sWAVES": "YiNbofFzC17jEHHCMwrRcpy9MrrjabMMLZxg8g5xmf7"} #, "XTN": "DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p" #,}
+
+for token in tokens:
+    decimals = 1e8 if token != "sNSBT" else 1e6
+    tokenId = tokens[token]
+
+    distr = loads(
+        get("https://nodes.wavesnodes.com/assets/"+tokenId+"/distribution/3999969/limit/1000").text)[
+        "items"]
+
+    users = {n: distr[n] for n in distr}
+    print(users)
+    poolAddresses = [pool["contractAddress"] for pool in pools if tokenId in [asset["assetId"] for asset in pool["assets"]]]
+    for address in poolAddresses:
+        poolBalance = loads(get("https://nodes-puzzle.wavesnodes.com/addresses/balance/"+address).text)["balance"] if tokenId == "WAVES" else  loads(get("https://nodes-puzzle.wavesnodes.com/assets/balance/{}?id={}".format(address, tokenId)).text)["balances"][0]["balance"]
+        data = loads(get("https://nodes-puzzle.wavesnodes.com/addresses/data/"+address).text)
+        dic = {n["key"]: n["value"] for n in data}
+
+        if not dic.get("global_indexStaked", 0) == 0:
+            for n in dic:
+                if "indexStaked" in n and not "global" in n:
+                    users[n.split("_")[0]] = users.get(n.split("_")[0], 0) + int(poolBalance * dic[n] / dic["global_indexStaked"])
+
+            print(address, poolAddresses.index(address) + 1)
+        else:
+            print("empty pool", address)
+
+        result["pools"][address] = "PuzzleSwap"
+
+    users = {n: users[n] / decimals for n in users if not users[n] < decimals and not n in result["pools"]}
+    result["assets"][token] = users
+
+with open("pswap-snsbt-users.json", "w") as f:
+    f.write(dumps(result, sort_keys=True, indent='\t', separators=(',', ': ')))
+
